328ef6c4e26042f3243978009f488dc33704d9be
carlos mur <cmurestudillos@gmail.com>
2020-09-30 08:51:25 +0200

    commit

--- src/components/articulos/Articulos.js
+++ src/components/articulos/Articulos.js
@@ -8,6 +8,8 @@ import Moment from 'react-moment';
 import 'moment/locale/es';
 // Carga la configuracion y URL BackEnd
 import Global from '../../api/Global';
+// Modelo
+import ArticuloModel from '../../models/ArticuloModel';
 // Imagenes
 import NoImage from '../../assets/img/noimage.png';
 
@@ -62,10 +64,11 @@ class Articulos extends Component{
         // Log de seguimiento
         console.log("Articulos.js - Metodo getUltimosArticulos");
 
-        axios.get(this.url + "articulos/last")
+        axios.get(this.url + "/articulos.json")
         .then(res => {
             this.setState({
-                articulos: res.data.articulos,
+                articulos: this.ordenarArray(res.data),
+                //articulos: res.data.articulos,
                 status: 'success'
             });
         });
@@ -88,6 +91,29 @@ class Articulos extends Component{
     }; 
 
     //----------------------------------------------------------------------//
+    // Metodo para mostrar los ultimos 5 articulos creados                  //
+    //----------------------------------------------------------------------//
+    ordenarArray(articulosArray){
+
+        var articulosData = [];
+        if(articulosArray === null){
+            return [];
+        }
+
+        Object.keys(articulosArray).reverse().forEach( key => {
+            var articulo = ArticuloModel; 
+            articulo = articulosArray[key];
+            if(articulosData.length <= 4){
+                articulo.id = key;
+                // Devolvemos en el Array el objeto extraido
+                articulosData.push(articulo);
+            }
+        });
+
+        return articulosData;
+    } 
+
+    //----------------------------------------------------------------------//
     // Metodo render()                                                      //
     //----------------------------------------------------------------------//
     render(){
@@ -99,11 +125,11 @@ class Articulos extends Component{
 
             var listarArticulos = this.state.articulos.map( (articulo) => {
                 return(
-                    <article key={articulo._id} className="article-item" id="article-template">
+                    <article key={articulo.id} className="article-item" id="article-template">
                         <div className="image-wrap">
                             {
                                 articulo.imagen !== null ? (
-                                   <img src={this.url + 'get-imagen/' + articulo.imagen} alt={articulo.titulo} />
+                                   <img src={articulo.imagen} alt={articulo.titulo} />
                                 ):(
                                     <img src={NoImage} alt="sin imagen" title="sin imagen" />
                                 )
@@ -116,7 +142,7 @@ class Articulos extends Component{
                                 {articulo.fecha}
                             </Moment>
                             </span>
-                        <Link to={'/blog/articulo/' + articulo._id} >Leer mas ...</Link>
+                        <Link to={'/blog/articulo/' + articulo.id} >Leer mas ...</Link>
                         {/* Limpiar los float */}
                         <div className="clearfix"></div>
                     </article>
@@ -131,7 +157,6 @@ class Articulos extends Component{
         }else if(this.state.articulos.length === 0 && this.state.status === null){
             return(
                 <div id="articles">
-                    <h2 className="sub-header">No hay articulos a mostrar.</h2>
                     <p>No hay contenido en la sección.</p>
                </div>
             );
